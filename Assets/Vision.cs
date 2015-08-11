0db1b7af55e4c94b83cf5cf6ea016649d5d16a93
nosferator <nosferator@hotmail.fr>
2015-08-12 01:02:54 +0200

    Champ de vision
    Détection du joueur dans le champ de vision du garde

---
+++ Assets/Vision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vision : MonoBehaviour {
+
+	public GameObject player;
+
+	private int angleOfView = 50;
+	private float distVision = 6;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Vector2.Angle (-transform.right, player.transform.position - transform.position) < angleOfView)
+		{
+			RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, -transform.right, distVision);
+			if ((hits.Length > 1) && hits[1].collider.tag == "Player")
+			{
+				Debug.Log("Player VUE !");
+			}
+		}
+	}
+}
